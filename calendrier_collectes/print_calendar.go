d3ce49c6308b357703cf22394b91e516d09e547e
benjamin vanheuverzwijn <bvanheu@gmail.com>
2014-08-19 11:02:03 -0400

    A run of gofmt (thanks to @obilodeau)

--- calendrier_collectes/print_calendar.go
+++ calendrier_collectes/print_calendar.go
@@ -51,122 +51,130 @@ import "io/ioutil"
 import "time"
 
 type Period struct {
-    Number string
-    Types string
-    Date_begin time.Time
-    Date_end time.Time
-    Information string
+	Number      string
+	Types       string
+	Date_begin  time.Time
+	Date_end    time.Time
+	Information string
 }
 
 type Calendar struct {
-    District_name string
-    Periods []Period
+	District_name string
+	Periods       []Period
 }
 
 type CollecteMatieresResiduelle struct {
-    Municipality_id string `json:"MUNID"`
-    Code_id string `json:"CODEID"`
-    Week_number string `json:"NO_SEM"`
-    Date_begin string `json:"DT01"`
-    Date_end string `json:"DT02"`
-    District string `json:"ARROND"`
-    Type string `json:"TYPE"`
-    Description string `json:"DESC"`
-    Information string `json:"INFO"`
+	Municipality_id string `json:"MUNID"`
+	Code_id         string `json:"CODEID"`
+	Week_number     string `json:"NO_SEM"`
+	Date_begin      string `json:"DT01"`
+	Date_end        string `json:"DT02"`
+	District        string `json:"ARROND"`
+	Type            string `json:"TYPE"`
+	Description     string `json:"DESC"`
+	Information     string `json:"INFO"`
 }
 
 // Json structure
 type jsonobject struct {
-    CalendrierCollectes struct {
-        CollecteMatieresResiduelles []CollecteMatieresResiduelle `json:"COLLECTE_MATIERES_RESIDUELLES"`
-    } `json:"CALENDRIER_COLLECTES"`
+	CalendrierCollectes struct {
+		CollecteMatieresResiduelles []CollecteMatieresResiduelle `json:"COLLECTE_MATIERES_RESIDUELLES"`
+	} `json:"CALENDRIER_COLLECTES"`
 }
 
 func usage() {
-    fmt.Printf("Usage: %s [OPTION] FILE.json\n\n", path.Base(os.Args[0]))
+	fmt.Printf("Usage: %s [OPTION] FILE.json\n\n", path.Base(os.Args[0]))
 }
 
 func init_log() {
-    log.SetPrefix(path.Base(os.Args[0]) + " - ")
-    log.SetOutput(os.Stdout)
+	log.SetPrefix(path.Base(os.Args[0]) + " - ")
+	log.SetOutput(os.Stdout)
 }
 
 func main() {
-    var json_buffer []byte
-    var m jsonobject
-    var err error
+	var json_buffer []byte
+	var m jsonobject
+	var err error
 
-    init_log()
+	init_log()
 
-    if len(os.Args) != 2 {
-        usage()
-        log.Fatal("not enough arguments")
-    }
+	if len(os.Args) != 2 {
+		usage()
+		log.Fatal("not enough arguments")
+	}
 
-    // Read the JSON file
-    json_buffer, err = ioutil.ReadFile(os.Args[1])
-    if err != nil { log.Fatal(err) }
+	// Read the JSON file
+	json_buffer, err = ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // JSON -> Objects
-    err = json.Unmarshal(json_buffer, &m)
-    if err != nil { log.Fatal(err) }
+	// JSON -> Objects
+	err = json.Unmarshal(json_buffer, &m)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Objects -> calendar
-    var previous_cmr CollecteMatieresResiduelle = m.CalendrierCollectes.CollecteMatieresResiduelles[0]
-    var montbellevue Calendar
-    var period Period
+	// Objects -> calendar
+	var previous_cmr CollecteMatieresResiduelle = m.CalendrierCollectes.CollecteMatieresResiduelles[0]
+	var montbellevue Calendar
+	var period Period
 
-    for _, cmr := range m.CalendrierCollectes.CollecteMatieresResiduelles {
-        if cmr.District == "Arrondissement du Mont-Bellevue" {
-            if cmr.Week_number != previous_cmr.Week_number {
-                // Flush the current period in the slice
-                montbellevue.Periods = append(montbellevue.Periods, period)
-                // Start a new period
-                period.Types = ""
-                period.Information = ""
-            }
+	for _, cmr := range m.CalendrierCollectes.CollecteMatieresResiduelles {
+		if cmr.District == "Arrondissement du Mont-Bellevue" {
+			if cmr.Week_number != previous_cmr.Week_number {
+				// Flush the current period in the slice
+				montbellevue.Periods = append(montbellevue.Periods, period)
+				// Start a new period
+				period.Types = ""
+				period.Information = ""
+			}
 
-            period.Number = cmr.Week_number
-            period.Date_begin, err = time.Parse("2006-01-02", cmr.Date_begin)
-            if err != nil { log.Fatal(err) }
-            period.Date_end, err = time.Parse("2006-01-02", cmr.Date_end)
-            if err != nil { log.Fatal(err) }
-            period.Types += cmr.Type
-            period.Information += cmr.Information
+			period.Number = cmr.Week_number
+			period.Date_begin, err = time.Parse("2006-01-02", cmr.Date_begin)
+			if err != nil {
+				log.Fatal(err)
+			}
+			period.Date_end, err = time.Parse("2006-01-02", cmr.Date_end)
+			if err != nil {
+				log.Fatal(err)
+			}
+			period.Types += cmr.Type
+			period.Information += cmr.Information
 
-            previous_cmr = cmr
-        }
-    }
-    // Flush the last entry
-    montbellevue.Periods = append(montbellevue.Periods, period)
+			previous_cmr = cmr
+		}
+	}
+	// Flush the last entry
+	montbellevue.Periods = append(montbellevue.Periods, period)
 
-    // Format the parsed data
-    var previous_time,current_time string
+	// Format the parsed data
+	var previous_time, current_time string
 
-    for _, p := range montbellevue.Periods {
-        current_time = (p.Date_begin.Format("January 2006"))
-        if current_time != previous_time {
-            fmt.Println("\n\n" + current_time)
-            previous_time = current_time
-        }
+	for _, p := range montbellevue.Periods {
+		current_time = (p.Date_begin.Format("January 2006"))
+		if current_time != previous_time {
+			fmt.Println("\n\n" + current_time)
+			previous_time = current_time
+		}
 
-        // Print week number - types - date begin - date end
-        fmt.Println(p.Number +
-            "\t" + p.Types +
-            "\t" + p.Date_begin.Format("01-02") +
-            "\t" + p.Date_end.Format("01-02"))
+		// Print week number - types - date begin - date end
+		fmt.Println(p.Number +
+			"\t" + p.Types +
+			"\t" + p.Date_begin.Format("01-02") +
+			"\t" + p.Date_end.Format("01-02"))
 
-        if p.Information != "" {
-            fmt.Println(" `-> " + p.Information)
-        }
-    }
+		if p.Information != "" {
+			fmt.Println(" `-> " + p.Information)
+		}
+	}
 
-    fmt.Println("\nLegende\n" +
-        "D - Déchets\n" +
-        "R - Récupération\n" +
-        "C - Compost\n" +
-        "S - Sapin\n" +
-        "E - Encombrant et bois\n" +
-        "B - Carton\n" +
-        "F - Feuilles mortes")
+	fmt.Println("\nLegende\n" +
+		"D - Déchets\n" +
+		"R - Récupération\n" +
+		"C - Compost\n" +
+		"S - Sapin\n" +
+		"E - Encombrant et bois\n" +
+		"B - Carton\n" +
+		"F - Feuilles mortes")
 }
